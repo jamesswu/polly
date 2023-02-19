8e7454b1c48a148f68343edb8fd915a10037b307
James Wu <wu.james@hotmail.com>
2023-02-19 13:24:28 -0500

    trpc procedures working, able to extract data from database

--- src/server/api/routers/question.ts
+++ src/server/api/routers/question.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
-export const exampleRouter = createTRPCRouter({
+export const questionRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -12,10 +12,20 @@ export const exampleRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.question.findMany();
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+  addQuestion: protectedProcedure
+    .input(z.object({question: z.string().min(5).max(500)}))
+    .mutation(({ctx,input}) => {
+      return ctx.prisma.question.create({
+        data: {
+          question: input.question,
+          options: [],
+        } 
+      })
+    }),
 });
